f1225539ffaa8a7d5889e51145e2e5ac408847d6
Morgan Hondros <morganhondros@gmail.com>
2018-12-29 17:57:20 -0500

    First commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Morgan Hondros on 12/29/18.
+//  Copyright © 2018 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
